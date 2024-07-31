8b98c8897b965e422690c3c24e12b13f111e6b65
jassem ben ali <85318125+jassemb@users.noreply.github.com>
2024-07-31 12:02:58 +0100

    Update 0-release.sh

--- cybersecurity_basics/0x00_introduction_cybersecurity/0-release.sh
+++ cybersecurity_basics/0x00_introduction_cybersecurity/0-release.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-lsb_release -a | grep "Distributor ID:" | cut -d ":" -f2 | tr -d "[:space:]" && printf "\n"
+lsb_release -a | grep "Distributor ID:" | cut -d ":" -f2 | tr -d "[:space:]" | sed '$a\'
