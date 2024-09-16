035b507506824277a57d6386cd15d6b07819e850
jassemb <benalijassem201@gmail.com>
2024-09-16 16:07:50 +0100

    test

--- web_application_security/0x03_protocols_servers/9-firewall.sh
+++ web_application_security/0x03_protocols_servers/9-firewall.sh
@@ -1,11 +1,2 @@
 #!/bin/bash
-
-iptables -F
-
-iptables -P INPUT DROP
-
-iptables -A INPUT -p tcp --dport 22 -j ACCEPT
-
-iptables -P OUTPUT ACCEPT
-
-iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+iptables -F && iptables -P INPUT DROP && iptables -A INPUT -p tcp --dport 22 -j ACCEPT && iptables -P OUTPUT ACCEPT && iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
