d75351c9f213b81f9475086fa3d5f3ab6fc064d1
jassemb <benalijassem201@gmail.com>
2024-09-25 10:46:15 +0100

    test

---
+++ 0x00-ruby_scripting/6-get.rb
@@ -0,0 +1,16 @@
+# 8-get.rb
+
+require 'net/http'
+require 'uri'
+require 'json'
+
+def get_request(url)
+  uri = URI.parse(url)
+  response = Net::HTTP.get_response(uri)
+
+  puts "Response status: #{response.code} #{response.message}"
+  puts "Response body:"
+  puts JSON.pretty_generate(JSON.parse(response.body))
+rescue JSON::ParserError
+  puts response.body
+end
