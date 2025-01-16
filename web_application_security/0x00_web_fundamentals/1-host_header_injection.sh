cf3563e152ae495d9c47597308efcc2a93c6aabf
jassemb <benalijassem201@gmail.com>
2025-01-16 15:03:23 +0100

    test

--- web_application_security/0x00_web_fundamentals/1-host_header_injection.sh
+++ web_application_security/0x00_web_fundamentals/1-host_header_injection.sh
@@ -1,16 +1,2 @@
 #!/bin/bash
-
-# Check if the correct number of arguments is provided
-if [ "$#" -ne 3 ]; then
-  echo "Usage: $0 NEW_HOST TARGET_URL FORM_DATA"
-  exit 1
-fi
-
-NEW_HOST="$1"
-TARGET_URL="$2"
-FORM_DATA="$3"
-
-# Make the request with a custom Host header
-curl -X POST "$TARGET_URL" \
-  -H "Host: $NEW_HOST" \
-  -d "$FORM_DATA"
+[ "$#" -ne 3 ] && { echo "Usage: $0 NEW_HOST TARGET_URL FORM_DATA"; exit 1; } || curl -X POST "$2" -H "Host: $1" -d "$3"
